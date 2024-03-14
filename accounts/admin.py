f87ab6356e8bcb3b9d963e9be7732863059573f7
EchoFoe <aspirpd@gmail.com>
2024-03-14 16:26:36 +0400

    Accounts: рефакторинг представления (swagger), админки

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,14 +8,15 @@ class AccountAdmin(admin.ModelAdmin):
     """ Админ-панель аккаунта """
 
     save_as = True
-    list_display = ['username', 'is_active', 'is_staff', 'is_superuser']
-    list_display_links = ['username']
+    list_display = ['first_name', 'username', 'is_active', 'is_staff', 'is_superuser']
+    list_display_links = ['first_name']
     list_filter = ['is_active', 'is_staff', 'is_superuser']
     search_fields = ['username', 'last_name', 'first_name']
+    readonly_fields = ['last_login', 'date_joined']
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Персональная информация', {'fields': (
-            'first_name', 'last_name', 'email',
+            'first_name', 'last_name', 'phone', 'telegram_chat_id',
         )}),
         ('Разрешения', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Даты', {'fields': ('last_login', 'date_joined')}),
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,15 +1,61 @@
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
-from accounts.models import Account
 from .serializers import AccountSerializer
+
+ACCOUNTS = 'Аккаунты (клиенты)'
 
 
 class AccountRegistrationAPIView(APIView):
+    @swagger_auto_schema(
+        tags=[ACCOUNTS],
+        request_body=AccountSerializer,
+        responses={
+            status.HTTP_201_CREATED: AccountSerializer(),
+            status.HTTP_400_BAD_REQUEST: 'Неверные данные были предоставлены'
+        },
+        operation_summary='Регистрация клиента',
+    )
     def post(self, request):
+        """
+        POST-запрос на регистрацию нового пользователя.
+
+        Параметры запроса включает в себя json со следующей структурой:
+        - first_name (строка): Имя пользователя.
+        - last_name (строка): Фамилия пользователя.
+        - phone (строка): Номер телефона пользователя.
+        - telegram_chat_id (строка): ID чата в Telegram.
+
+        Пример запроса:
+        -   {
+                "first_name": "Имя",
+                "last_name": "Фамилия",
+                "phone": "+1111111111",
+                "telegram_chat_id": "1111111111"
+            }
+
+        Пример ответа:
+        Если пользователь успешно создан, возвращается статус 201 CREATED и данные нового пользователя:
+        -   {
+                "id": 1,
+                "first_name": "Имя",
+                "last_name": "Фамилия",
+                "phone": "+1111111111",
+                "telegram_chat_id": "1111111111"
+            }
+
+        Если переданы некорректные данные, возвращается статус 400 BAD REQUEST и соответствующее сообщение об ошибке.
+        """
         serializer = AccountSerializer(data=request.data)
         if serializer.is_valid():
+            username = serializer.validated_data.get('username')
+            if not username:
+                User = get_user_model()
+                username = User.objects.make_random_password(length=10)
+                serializer.validated_data['username'] = username
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
